db43db92fa2643288d94394088c88d32e8da108e
Jocelle Bautista <jcllebautista@gmail.com>
2024-05-11 23:01:30 -0600

    Check JSON

--- FetchRecipeApp/Api.swift
+++ FetchRecipeApp/Api.swift
@@ -1,1 +1,6 @@
-// 
+// api = https://themealdb.com/api.php
+// endpoints = [
+    // https://themealdb.com/api/json/v1/1/filter.php?c=Dessert,
+    //  https://themealdb.com/api/json/v1/1/lookup.php?i=MEAL_ID
+    // ]
+
