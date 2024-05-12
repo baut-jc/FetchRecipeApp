7cf133d6cf64bbc203be6c9d73ab9f8deac3fbff
Jocelle Bautista <jcllebautista@gmail.com>
2024-05-11 22:30:07 -0600

    Inital commit

---
+++ FetchRecipeApp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  FetchRecipeApp
+//
+//  Created by Jocelle Jouaht on 5/10/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
